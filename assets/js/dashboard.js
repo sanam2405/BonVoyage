ed7bf04e8fbaba14cce1efd8e75755f6c0814335
Krishnaneel <krish.naneel11@gmail.com>
2023-08-02 05:06:52 +0530

    WIP user data getting stored in local storage

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,16 +1,25 @@
-const greeting = document.querySelector('.greeting');
+const usernameContainer = document.getElementById("usernameContainer");
+const userData = localStorage.getItem("userData");
+const parsedUserData = userData ? JSON.parse(userData) : null;
 
 window.onload = () => {
-    if(!sessionStorage.name){
-        location.href = '/';
-    } else{
-        greeting.innerHTML = `hello ${sessionStorage.name}`;
+    // if(!sessionStorage.name){
+    //     location.href = '/';
+    // } else{
+    //     greeting.innerHTML = `hello ${sessionStorage.name}`;
+    // }
+
+    if (parsedUserData && parsedUserData.username) {
+        usernameContainer.textContent = `Hello, ${parsedUserData.username}!`;
+    }else{
+        window.location.href = '/';
     }
+
 }
 
-const logOut = document.querySelector('.logout');
+// const logOut = document.querySelector('.logout');
 
-logOut.onclick = () => {
-    sessionStorage.clear();
-    location.reload();
-}
+// logOut.onclick = () => {
+//     sessionStorage.clear();
+//     location.reload();
+// }
