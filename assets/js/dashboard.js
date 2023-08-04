87ffa4952ae032a876903f7b22a467825c836c97
Krishnaneel <krish.naneel11@gmail.com>
2023-08-04 19:36:50 +0530

    WIP 4 JWT token added

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,25 +1,31 @@
+// const jwt = require("jsonwebtoken");
+// const Register = require("../models/registration");
+
+// const func = async(req,res) =>{
+//     try{
+
+//         const token = req.cookies.jwt;
+//         const verifyUser = jwt.verify(token,process.env.SECRET_KEY_JWT);
+//         const user = await Register.findOne({_id:verifyUser._id});
+//         console.log(user);
+
+//         req.token=token;
+//         req.user=user;
+
+//     }catch(error){
+//         res.status(401).send(error);
+//     }    
+// }
+
+
 const usernameContainer = document.getElementById("usernameContainer");
 const userData = localStorage.getItem("userData");
 const parsedUserData = userData ? JSON.parse(userData) : null;
 
-window.onload = () => {
-    // if(!sessionStorage.name){
-    //     location.href = '/';
-    // } else{
-    //     greeting.innerHTML = `hello ${sessionStorage.name}`;
-    // }
-
-    if (parsedUserData && parsedUserData.username) {
-        usernameContainer.textContent = `Hello, ${parsedUserData.username}!`;
-    }else{
-        window.location.href = '/';
-    }
-
+if (parsedUserData && parsedUserData.username) {
+    usernameContainer.textContent = `Hello, ${parsedUserData.username}!`;
+}else{
+    location.href("/index.html")
 }
 
-// const logOut = document.querySelector('.logout');
 
-// logOut.onclick = () => {
-//     sessionStorage.clear();
-//     location.reload();
-// }
