5f7fc6de034191e0bf880c984809e0f72fe11c35
Manas Pratim Biwas <manaspb2405@gmail.com>
2023-11-04 02:42:06 +0530

    added APIs to .env

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 80; // Choosing a port for your server
+const port = 80; // Choosing a port for the server
+const fs = require('fs');
+require("dotenv").config()
 
+const apiKey = process.env.GOOGLE_MAP_API_KEY;
 
 
 console.log(__dirname);
@@ -9,16 +12,29 @@ console.log(__dirname);
 // Serving static files from the 'assets' directory
 app.use(express.static('assets'));
 
-
-  
-
 // Defining your routes
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/navigation', (req, res) => {
-  res.sendFile(__dirname + '/navigation.html');
+  // Reading the HTML file
+  const filePath = `${__dirname}/navigation.html`;
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Error in reading the HTML file');
+    }
+
+    // Replacing a placeholder in the HTML with the API key
+    const htmlWithAPIKey = data.replace('API_KEY_PLACEHOLDER', apiKey);
+
+    // console.log(apiKey)
+    // console.log('htmlWithAPIKey:', htmlWithAPIKey);
+
+    // Sending the modified HTML as the response
+    res.send(htmlWithAPIKey);
+  });
 });
 
 // Starting the server
